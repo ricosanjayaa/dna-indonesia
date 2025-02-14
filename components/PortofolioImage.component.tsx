6b1dd13ad4ed3b989b64683a1c0a50a669c322da
ricosanjayaa <ricosanjayasan@gmail.com>
2025-02-14 09:13:27 +0700

    animation changes

--- components/PortofolioImage.component.tsx
+++ components/PortofolioImage.component.tsx
@@ -19,9 +19,11 @@ export default function PortofolioImage({ src, type }: { src: string, type: 1 | 
   }
 
   return (
-    <div className="relative w-full aspect-[4/5] rounded-md overflow-hidden group">
-      <img className="h-full aspect-[4/5] object-cover rounded-t-md rounded-b-md group-hover:rounded-b-none group-hover:-translate-y-10 transition-all duration-500 ease-in-out" src={src} alt={"Portofolio " + typeAfter + " DNA Indonesia"} />
-      <Text className="absolute bg-background w-full p-8 rounded-b-md group-hover:-translate-y-[84px] transition-all duration-1000 ease-in-out">Portofolio DNA | {typeAfter}</Text>
+    <div className="relative w-full aspect-[4/5] rounded-xl overflow-hidden group">
+      <img className="h-full aspect-[4/5] object-cover rounded-t-md rounded-b-md group-hover:rounded-b-none group-hover:scale-105 transition-all duration-700 ease-[cubic-bezier(0.4,0,0.2,1)]" src={src} alt={"Portofolio " + typeAfter + " DNA Indonesia"} />
+      <div className="absolute inset-x-0 -bottom-[84px] bg-background/70 backdrop-blur-lg p-8 rounded-b-md transform group-hover:-translate-y-[84px] transition-all duration-700 ease-[cubic-bezier(0.4,0,0.2,1)] delay-150">
+        <Text>Portofolio DNA | {typeAfter}</Text>
+      </div>
     </div>
   );
 }
